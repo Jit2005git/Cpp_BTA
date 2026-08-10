c1bed2dbeab057e57effab0204a68fd4aed59488
Jit2005git <jitghosh304@gmail.com>
2026-08-10 23:43:28 +0530

    2.4

--- loops/odd_1-10.cpp
+++ loops/odd_1-10.cpp
@@ -3,5 +3,9 @@ using namespace std;
     int main(){
         int n;
         cout<<"Enter a number: ";
-        
+        cin>>n;
+        cout<<"The odd numbers from 1 to "<<n<<" are: ";
+        for(int i=1;i<=n;i+=2){
+            cout<<i<<" ";
+        }
     }
