ac494b5e1a11c7d83f66fd606ce4c3ae38934ec7
Jit2005git <jitghosh304@gmail.com>
2026-08-16 19:47:16 +0530

    p2

--- pointer/basic.cpp
+++ pointer/basic.cpp
@@ -1,2 +1,11 @@
 #include<iostream>
 using namespace std;
+int main(){
+    int a=5;
+    int *p;
+    p=&a;
+    cout<<"Value of a: "<<a<<endl;
+    cout<<"Address of a: "<<&a<<endl;
+    cout<<"Value of p: "<<p<<endl;
+    cout<<"Value pointed by p: "<<*p<<endl;
+}
